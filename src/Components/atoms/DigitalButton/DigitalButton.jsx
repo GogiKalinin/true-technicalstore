7d7e100affed412a57c8b5c7b74bbb944585921b
GogiKalinin <bbdostoevskij@gmail.com>
2022-12-27 21:35:37 +0200

    add button for Checkout page and other components

---
+++ src/Components/atoms/DigitalButton/DigitalButton.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./DigitalButton.sass";
+
+const DigitalButton = (props) => {
+  return (
+    <button className="DigitalButton" onClick={props.onClick}
+    style={{
+      background: props.background ? props.background : 'white',
+      color: props.color ? props.color : '#0156FF',
+      border: props.border ? props.border : '1px solid #0156FF',
+      width: props.width ? props.width : '208px',
+      height: props.height ? props.height : '50px',
+    }}>
+      {props.text}
+    </button>
+  );
+};
+
+export default DigitalButton;
