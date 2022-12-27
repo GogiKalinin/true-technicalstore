7402647a88fe93e543b7b37512bb604a754746c8
GogiKalinin <bbdostoevskij@gmail.com>
2022-12-27 21:39:00 +0200

    add MiniBasket for Checkout page

---
+++ src/Components/molecules/MiniBasket/MiniBasket.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { useState } from "react";
+import { BasketArrow } from "../../../Images/Basket";
+import { Cancel } from "../../../Images/Product/ProductImages";
+import BasketInput from "../../BasketInput/BasketInput";
+import "./MiniBasket.sass";
+
+export const MiniBasket = () => {
+  const prepareTitle = () => {
+    return mini_basket_data.length < 2 ? (
+      <h1>{mini_basket_data.length}item in card</h1>
+    ) : (
+      <h1>{mini_basket_data.length}items in card</h1>
+    );
+  };
+  const mini_basket_data = JSON.parse(localStorage.getItem("basketData"));
+  console.log(mini_basket_data);
+  const [showBasketProducts, setShowBasketProducts] = useState(true);
+  return (
+    <div className="MiniBasket">
+      <div className="MiniBasketTitle">
+        <h1>Order Summary</h1>
+      </div>
+      <div
+        className="MiniBasketItemsTitle"
+        onClick={() => setShowBasketProducts(!showBasketProducts)}
+      >
+        {prepareTitle()}
+        {showBasketProducts ? (
+          <div className="TitleArrowUnactive">{BasketArrow}</div>
+        ) : (
+          <div className="TitleArrowActive">{BasketArrow}</div>
+        )}
+      </div>
+      {showBasketProducts && (
+        <div className="MiniBasketItemsContainer">
+          {mini_basket_data.map((prod) => {
+            return (
+              <div className="BasketItem" key={prod.id}>
+                <div className="BasketItemImage">
+                  <img src={prod.images[0]} alt="img"></img>
+                </div>
+                <div className="BasketItemAboutProd">
+                  <div className="BasketItemMain">
+                    <div className="BasketItemMainDescription">
+                      <span>{prod.description.slice(0, 55) + '...'}</span>
+                    </div>
+                    <div className="BasketItemMainCounterAndPrice">
+                      <BasketInput
+                        // onChange={(event) => changeInput(prod.id, event)}
+                        number={prod.count}
+                        // {...searchaItemCount}
+                        // onClick={(event) => changeCost(prod.id, event, prod.count)}
+                      />
+                      <h1>{prod.newPrice}555$</h1>
+                    </div>
+                  </div>
+                  <div
+                    className="BaskedProdCancel"
+                    // onClick={() => removeFromBasket(prod.id)}
+                  >
+                    {Cancel}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
