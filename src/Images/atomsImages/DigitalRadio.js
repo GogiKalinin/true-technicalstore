2006c2f7e4c09c0388d0add61b926e468f6a96f5
GogiKalinin <bbdostoevskij@gmail.com>
2022-12-29 21:46:21 +0200

    add svg for DigitalRadio

---
+++ src/Images/atomsImages/DigitalRadio.js
@@ -0,0 +1,37 @@
+export const activeRadio = (
+  <svg
+    width="21"
+    height="21"
+    viewBox="0 0 21 21"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle
+      cx="10.5"
+      cy="10.5"
+      r="9.5"
+      fill="white"
+      stroke="#0156FF"
+      stroke-width="2"
+    />
+    <circle cx="10.5" cy="10.5" r="6.5" fill="#0156FF" />
+  </svg>
+);
+export const unactiveRadio = (
+  <svg
+    width="21"
+    height="21"
+    viewBox="0 0 21 21"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle
+      cx="10.5"
+      cy="10.5"
+      r="9.5"
+      fill="white"
+      stroke="#0156FF"
+      stroke-width="2"
+    />
+  </svg>
+);
