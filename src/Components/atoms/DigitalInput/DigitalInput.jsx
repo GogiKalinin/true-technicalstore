bee80b1b1bdba55fe5102c67efd605c245c04cfb
GogiKalinin <bbdostoevskij@gmail.com>
2022-12-30 22:07:08 +0200

    add test drop-down mode to DigitalInput

--- src/Components/atoms/DigitalInput/DigitalInput.jsx
+++ src/Components/atoms/DigitalInput/DigitalInput.jsx
@@ -1,14 +1,52 @@
-import React from 'react';
-import './DigitalInput.sass'
+import React from "react";
+import { useState } from "react";
+import "./DigitalInput.sass";
 
-export const DigitalInput = ({text, height, width}) => {
+export const DigitalInput = ({ text, height, width, dropdown, listArray }) => {
+  const [openDropdownList, setOpenDropdownList] = useState(false)
+  //функция по поиску клацнутого варианта списка 
+  //и помещению оного в див DigitalInputDropdownModeMain
+  //с дальнейшим сохранием в localStorage
+  const choseListVariant = (test) => {
+    console.log(test.name)
+    return (
+      test.name
+      )
+  }
+// document.getElementById("DropdownMode").value = choseListVariant;
+  
   return (
-    <div className='DigitalInput'>
-      <h1 className='DigitalInputTitle'>{text}<span>*</span></h1>
-      <input className='DigitalInputInput'
-      style={{
-      }}
-      ></input>
-    </div>
-  )
-}
+    <>
+      {dropdown ? (
+        <div include="DigitalInputDropdownMode">
+          <h1 className="DigitalInputDropdownModeTitle">
+            
+            <span>*</span>
+          </h1>
+          <div className="DigitalInputDropdownModeMain" id="DropdownMode" onClick={()=>setOpenDropdownList(!openDropdownList)}>
+            {choseListVariant}
+          </div>
+          {openDropdownList &&
+            <div className="DigitalInputDropdownModeMainList">
+              {listArray.map((test) => {
+                return (
+                  <div className="DigitalInputDropdownModeMainListItem" key={test.id} onClick={()=>choseListVariant(test, listArray)}>
+                    <p>{test.name}</p>
+                  </div>
+                )
+              })}
+            </div>
+          }
+        </div>
+      ) : (
+        <div className="DigitalInput">
+          <h1 className="DigitalInputTitle">
+            {text}
+            <span>*</span>
+          </h1>
+          <input className="DigitalInputInput" style={{}}></input>
+        </div>
+      )}
+    </>
+  );
+};
