daa4c8ff3f64c4bf1508c8af4190b5941918e70c
GogiKalinin <bbdostoevskij@gmail.com>
2023-01-01 21:47:42 +0200

    some little fixes for dropdown mode

--- src/Components/atoms/DigitalInput/DigitalInput.jsx
+++ src/Components/atoms/DigitalInput/DigitalInput.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { InputArrow } from "../../../Images/Product/ProductImages";
 import "./DigitalInput.sass";
 
 export const DigitalInput = ({ text, height, width, dropdown, listArray }) => {
@@ -7,30 +8,35 @@ export const DigitalInput = ({ text, height, width, dropdown, listArray }) => {
   //функция по поиску клацнутого варианта списка 
   //и помещению оного в див DigitalInputDropdownModeMain
   //с дальнейшим сохранием в localStorage
-  const choseListVariant = (test) => {
-    console.log(test.name)
-    return (
-      test.name
-      )
+  const choseListVariant = (id) => {
+    console.log(id)
+    // return (
+    //   test.name
+    //   )
   }
-// document.getElementById("DropdownMode").value = choseListVariant;
   
   return (
     <>
       {dropdown ? (
         <div include="DigitalInputDropdownMode">
           <h1 className="DigitalInputDropdownModeTitle">
-            
+            {text}
             <span>*</span>
           </h1>
           <div className="DigitalInputDropdownModeMain" id="DropdownMode" onClick={()=>setOpenDropdownList(!openDropdownList)}>
-            {choseListVariant}
+            <div className="DigitalInputDropdownModeMainChosenText">
+              {listArray[0].name}
+            </div>
+            {/* {choseListVariant} */}
+            <div className={openDropdownList ? "DigitalInputDropdownModeMainArrowActive" : 'DigitalInputDropdownModeMainArrowUnactive'}>
+              {InputArrow}
+            </div>
           </div>
           {openDropdownList &&
             <div className="DigitalInputDropdownModeMainList">
               {listArray.map((test) => {
                 return (
-                  <div className="DigitalInputDropdownModeMainListItem" key={test.id} onClick={()=>choseListVariant(test, listArray)}>
+                  <div className="DigitalInputDropdownModeMainListItem" key={test.id} onClick={() => choseListVariant(id)}>
                     <p>{test.name}</p>
                   </div>
                 )
