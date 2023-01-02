316be187044dd2f1971622b7e0f62fd353ef5c46
GogiKalinin <bbdostoevskij@gmail.com>
2023-01-02 21:23:52 +0200

    some upgrades for map in Checkout page

--- src/Components/atoms/DigitalInput/DigitalInput.jsx
+++ src/Components/atoms/DigitalInput/DigitalInput.jsx
@@ -3,54 +3,91 @@ import { useState } from "react";
 import { InputArrow } from "../../../Images/Product/ProductImages";
 import "./DigitalInput.sass";
 
-export const DigitalInput = ({ text, height, width, dropdown, listArray }) => {
-  const [openDropdownList, setOpenDropdownList] = useState(false)
-  //функция по поиску клацнутого варианта списка 
+export const DigitalInput = ({ 
+  title, 
+  dropdown, 
+  type,
+  height, 
+  width, 
+ }) => {
+  const [openDropdownList, setOpenDropdownList] = useState(false);
+  //функция по поиску клацнутого варианта списка
   //и помещению оного в див DigitalInputDropdownModeMain
   //с дальнейшим сохранием в localStorage
-  const choseListVariant = (id) => {
-    console.log(id)
-    // return (
-    //   test.name
-    //   )
-  }
-  
+  // const choseListVariant = (id) => {
+  //   if (id) {
+  //     const index = id - 1;
+  //     localStorage.setItem(
+  //       "chosenListItem",
+  //       JSON.stringify(listArray[index].name)
+  //     );
+  //   }
+  // };
+  // console.log(JSON.parse(localStorage.getItem("chosenListItem")));
+
+  // const getInputValues = () => {};
+
   return (
     <>
-      {dropdown ? (
+      {dropdown === true ? (
         <div include="DigitalInputDropdownMode">
+          {/* {listArray.map((arr) => {
+            return ( */}
           <h1 className="DigitalInputDropdownModeTitle">
-            {text}
+            {title}
             <span>*</span>
           </h1>
-          <div className="DigitalInputDropdownModeMain" id="DropdownMode" onClick={()=>setOpenDropdownList(!openDropdownList)}>
+            {/* )
+          })} */}
+          <div
+            className="DigitalInputDropdownModeMain"
+            id="DropdownMode"
+            onClick={() => setOpenDropdownList(!openDropdownList)}
+          >
             <div className="DigitalInputDropdownModeMainChosenText">
-              {listArray[0].name}
+              {/* {listArray[0].name} */}
+              {/* <h1>{(listArray)}</h1> */}
+              {JSON.parse(localStorage.getItem("chosenListItem"))}
             </div>
-            {/* {choseListVariant} */}
-            <div className={openDropdownList ? "DigitalInputDropdownModeMainArrowActive" : 'DigitalInputDropdownModeMainArrowUnactive'}>
+            <div
+              className={
+                openDropdownList
+                  ? "DigitalInputDropdownModeMainArrowActive"
+                  : "DigitalInputDropdownModeMainArrowUnactive"
+              }
+            >
               {InputArrow}
             </div>
           </div>
-          {openDropdownList &&
+          {openDropdownList && (
             <div className="DigitalInputDropdownModeMainList">
-              {listArray.map((test) => {
-                return (
-                  <div className="DigitalInputDropdownModeMainListItem" key={test.id} onClick={() => choseListVariant(id)}>
-                    <p>{test.name}</p>
+              {/* {listArray.map((test) => { */}
+                 {/* return (  */}
+                  <div
+                    className="DigitalInputDropdownModeMainListItem"
+                    // key={test.id}
+                    // onClick={() => choseListVariant(test.id)}
+                  >
+                    {/* <p>{test.name}</p>  */}
                   </div>
-                )
-              })}
+                 {/* );  */}
+               {/* })}  */}
             </div>
-          }
+          )}
         </div>
       ) : (
         <div className="DigitalInput">
-          <h1 className="DigitalInputTitle">
-            {text}
-            <span>*</span>
-          </h1>
-          <input className="DigitalInputInput" style={{}}></input>
+          {/* {listArray.map((test) => { */}
+            {/* return ( */}
+              <>
+                <h1 className="DigitalInputTitle">
+                  {title}
+                  <span>*</span>
+                </h1>
+                <input className="DigitalInputInput" style={{}}></input>
+              </>
+             {/* );  */}
+           {/* })}  */}
         </div>
       )}
     </>
