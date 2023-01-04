e9382042e9d4f91431431e293b0b44e83e2d6e5a
GogiKalinin <bbdostoevskij@gmail.com>
2023-01-04 21:52:29 +0200

    formated document

--- src/Components/atoms/DigitalRadio/DigitalRadio.jsx
+++ src/Components/atoms/DigitalRadio/DigitalRadio.jsx
@@ -5,26 +5,17 @@ import {
 } from "../../../Images/atomsImages/DigitalRadio";
 import "./DigitalRadio.sass";
 
-export const DigitalRadio = ({title, image, conditions, price}) => {
+export const DigitalRadio = ({ title, image, conditions, price }) => {
   return (
     <div className="DigitalRadio">
       <div className="DigitalRadioTitle">{title}</div>
       <div className="DigitalRadioMain">
         <div className="DigitalRadioConditions">
-            <div className="DigitalRadioConditionsRadio">
-                {image}
-            </div>
-            <div className="DigitalRadioConditionsText">
-                {conditions} 
-            </div>
-            
+          <div className="DigitalRadioConditionsRadio">{image}</div>
+          <div className="DigitalRadioConditionsText">{conditions}</div>
         </div>
-        <div className="DigitalRadioPrice">
-            {price}
-        </div>
+        <div className="DigitalRadioPrice">{price}</div>
       </div>
-
-      {/* {activeRadio} */}
     </div>
   );
 };
